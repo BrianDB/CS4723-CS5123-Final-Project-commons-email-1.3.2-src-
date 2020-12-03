efa661b9593824fc0a5a5fb869370f1d8dcb738a
BrianDB <briandb_13@yahoo.com>
2020-12-02 20:54:52 -0600

    Added more test methods

--- src/test/java/org/apache/commons/mail/TestCaseOne.java
+++ src/test/java/org/apache/commons/mail/TestCaseOne.java
@@ -11,10 +11,73 @@ public class TestCaseOne extends TestCase{
 	public void setUp() {
 		testEmail = new SimpleEmail();
 	}
+
 	public void testAddCc() throws EmailException {
-		System.out.println("This is a message from the test case one.");
+		System.out.println("Test to add CC Addresses - jyb___");
 		testEmail.addCc("a@b.com");
 		assertEquals("a@b.com", testEmail.getCcAddresses().get(0).toString());
 	}
+
+	/**
+     	* testAddCcMultiple
+     	* Adds multiple emails to the CcAddress list
+     	* Checks if either of those emails are in the list
+     	* @throws EmailException
+     	*/
+    	@Test
+    	public void testAddCcMultiple() throws EmailException {
+        	System.out.println("Test to add multiple CC addresses - jyb___"); 
+		String [] testEmails = {"a@b.com", "b@c.com"};
+        	testEmail.addCc(testEmails);
+        	assertEquals("a@b.com", testEmail.getCcAddresses().get(0).toString());
+        	assertEquals("b@c.com", testEmail.getCcAddresses().get(1).toString());
+    	}
+
+	/**
+     	* testAddHeader
+     	* Adds a new header, and checks if the header is in the header map
+     	* @throws IllegalArgumentException
+     	*/
+    	@Test
+    	public void testAddHeader() throws IllegalArgumentException {
+        	System.out.println("Test to add header - jyb___"); 
+		testEmail.addHeader("Header", "1");
+        	Map<String, String> hash = new HashMap<>(); 
+        	hash.put("Header", "1");
+        	assertEquals(hash, testEmail.headers); 
+    	}
+    
+	/**
+     	* testGetSentDate
+     	* checks to see if I can set the date
+     	* also checks to see if I can set a null date
+    	*/
+	@Test
+    	public void testGetSentDate()  {
+		System.out.println("Test to set the date - jyb___"); 
+        	Date date = new Date(); 
+        	testEmail.setSentDate(date);
+        	assertEquals(date, testEmail.getSentDate()); 
+        
+        	Date secondDate = null; 
+        	testEmail.setSentDate(secondDate);
+        	assertEquals(date, testEmail.getSentDate()); 
+    	}
+    
+      	/**
+     	* testSend
+     	* sends an email and checks to see the subject of it
+     	* @throws EmailException
+     	*/
+    	@Test
+    	public void testSend() throws EmailException {
+		System.out.println("Test to send an email - jyb___"); 
+        	testEmail.setHostName("utsa.com");
+        	testEmail.setFrom("a@b.com");
+        	testEmail.addTo("b@c.com"); 
+        	testEmail.setSubject("Subject");
+        	testEmail.send();
+        	assertEquals("Subject", testEmail.getSubject().toString()); 
+   	}
 
 }
--- src/test/java/org/apache/commons/mail/TestCaseTwo.java
+++ src/test/java/org/apache/commons/mail/TestCaseTwo.java
@@ -12,9 +12,38 @@ public class TestCaseTwo extends TestCase{
 		testEmail = new SimpleEmail();
 	}
 	public void testAddCc() throws EmailException {
-		System.out.println("This is a message from the test case two.");
+		System.out.println("Test to add CC address - jyb___");
 		testEmail.addCc("a@b.com");
 		assertEquals("a@b.com", testEmail.getCcAddresses().get(0).toString());
-	}
+	}  
+	
+	/**
+     	* testAddBccMultiple
+     	* Adds multiple emails to the BccAddress list
+     	* Checks if either of those emails are in the list
+     	* @throws EmailException
+     	*/
+    	@Test
+    	public void testAddBccMultiple() throws EmailException {
+		System.out.println("Test to add multiple BCC addresses - jyb___"); 
+        	String [] testEmails = {"a@b.com", "b@c.com"}; 
+        	testEmail.addBcc(testEmails);
+        	assertEquals("a@b.com", testEmail.getBccAddresses().get(0).toString()); 
+        	assertEquals("b@c.com", testEmail.getBccAddresses().get(1).toString()); 
+    	}
+
+	/**
+     	* testGetMailSessionWithHostName
+     	* sets a hostname and gets the mail session, checks for an empty hostname
+     	* @throws EmailException
+    	*/
+    	@Test
+    	public void testGetMailSessionWithHostName() throws EmailException {
+		System.out.println("Test to get a mail session - jyb___")
+        	testEmail.setHostName("utsa.com");
+        	testEmail.getMailSession(); 
+        	assertFalse(EmailUtils.isEmpty(testEmail.hostName)); 
+    	}
+
 
 }
